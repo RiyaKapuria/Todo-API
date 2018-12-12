16b7cf386739d9441b0fe18378d40da61a7eec7e
RiyaKapuria <riyakapuria.1992@gmail.com>
2018-12-12 19:28:11 +0530

    Create a Test Database

--- server/db/mangoose.js
+++ server/db/mangoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/TodoApp';
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || url);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
