add11c2ad6c6954b647ed2c86f7783355b20600f
RiyaKapuria <riyakapuria.1992@gmail.com>
2018-12-12 16:33:34 +0530

    configure for heroku

--- server/db/mangoose.js
+++ server/db/mangoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
+mongoose.connect(process.env.MONGODB_URI || url);
 
 module.exports = {mongoose};
